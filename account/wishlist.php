117c55f5ced65a5f167b7be66c8b05e17efb6124
farukham <farukham@gmail.com>
2014-05-21 03:15:16 +0600

    third commit

---
+++ account/wishlist.php
@@ -0,0 +1,41 @@
+<?php
+
+// Heading 
+
+$_['heading_title'] = 'আমার পছেন্দের তালিকা';
+
+
+
+// Text
+
+$_['text_account']  = 'অ্যাকাউন্ট';
+
+$_['text_instock']  = 'ইন স্টক';
+
+$_['text_wishlist'] = 'পছেন্দের তালিকা (%s)';
+
+$_['text_login']    = 'আপনার <a href="%s">পছেন্দের তালিকায়</a> সেভ করার জন্যে আপনাকে <a href="%s">লগইন</a> অথবা <a href="%s">অ্যাকাউন্ট তৈরি করতে হবে</a>!';
+
+$_['text_success']  = 'সাফল্য: আপনি <a href="%s">%s</a> আপনার <a href="%s">পছেন্দের তালিকায়</a> যোগ করেছেন!';
+
+$_['text_remove']   = 'সাফল্য: পছেন্দের তালিকা পরিবর্তন করেছেন।';
+
+$_['text_empty']    = 'পছেন্দের তালিকা খালি';
+
+
+
+// Column
+
+$_['column_image']  = 'Image';
+
+$_['column_name']   = 'Product Name';
+
+$_['column_model']  = 'Model';
+
+$_['column_stock']  = 'Stock';
+
+$_['column_price']  = 'Unit Price';
+
+$_['column_action'] = 'Action';
+
+?>
---
+++ common/footer.php
@@ -0,0 +1,18 @@
+<?php
+// Text
+$_['text_information']  = 'তথ্য';
+$_['text_service']      = 'গ্রাহক সেবা';
+$_['text_extra']        = 'অতিরিক্ত';
+$_['text_contact']      = 'যোগাযোগ';
+$_['text_return']       = 'ফেরত';
+$_['text_sitemap']      = 'সাইট মাপ';
+$_['text_manufacturer'] = 'ব্রান্ড';
+$_['text_voucher']      = 'গিফট ভাউচার';
+$_['text_affiliate']    = 'আফিলেটস';
+$_['text_special']      = 'স্পেশাল';
+$_['text_account']      = 'আমার অ্যাকাউন্ট';
+$_['text_order']        = 'আপনার অর্ডার ইতিহাস দেখুন';
+$_['text_wishlist']     = 'আপনার ইচ্ছা তালিকা পরিবর্তন করুন';
+$_['text_newsletter']   = 'সদস্যতা / নিউজলেটার সদস্যতা ত্যাগ';
+$_['text_powered']      = 'Powered By <a href="http://www.opencart.com">ফলমুল ডটকম</a><br /> %s &copy; %s';
+?>
